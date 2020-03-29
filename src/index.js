99156fe4d47fc14f53fef15a1a389c41045eb805
andr999 <andreykuchay2@gmail.com>
2020-03-29 12:54:08 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from 'routes'
+import Topbar from 'components/topBar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+import  CurrentUserChecker  from 'components/currentUserChecker'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return (
+    <CurrentUserProvider>
+        <CurrentUserChecker>
+          <Router>
+            <Topbar />
+            <Routes />
+          </Router>
+        </CurrentUserChecker>
+    </CurrentUserProvider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
